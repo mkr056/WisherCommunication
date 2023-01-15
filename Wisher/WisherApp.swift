356c04db77eb7fffc328830bace17bf08093d19d
Artem M <mkrlvi56@gmail.comm>
2023-01-15 14:35:57 +0400

    Initial Commit

---
+++ Wisher/WisherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WisherApp.swift
+//  Wisher
+//
+//  Created by Artem Mkr on 15.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct WisherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
